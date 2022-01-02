eafe740965ea2576e2cf42696d9ea23d4da31a42
Nirajan Mahara <N.Mahara@wlv.ac.uk>
2022-01-02 23:06:01 +0545

    react hooks `{useState, useEffect}`

--- src/MyRoute.js
+++ src/MyRoute.js
@@ -1,26 +1,26 @@
 import React from 'react'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import IncrementDecrement from './hooks/IncrementDecrement'
 import HomePage from './pages/HomePage'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
-
+import Test from './hooks/Test'
 
 const MyRoute = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage/>} />
-                <Route path="/signup" element={<Signup/>}/>
-                <Route path="/signin" element={<Signin/>}/>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
 
-                {/* hooks */}
-                <Route path="/increment" element={<IncrementDecrement/>} />
+        {/* hooks */}
+        <Route path='/increment' element={<IncrementDecrement />} />
 
-            </Routes>
-            
-        </Router>
-    )
+        <Route path='/hooks' element={<Test />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default MyRoute
