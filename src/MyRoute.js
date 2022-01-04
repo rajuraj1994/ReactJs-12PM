b67f8aad33e1495ab97030d669b565ee36ab0617
Nirajan Mahara <N.Mahara@wlv.ac.uk>
2022-01-04 07:14:26 +0545

    The React Context API is a way for a React app to effectively produce global variables that can be passed around, without having to pass props down manually at every level.

--- src/MyRoute.js
+++ src/MyRoute.js
@@ -8,6 +8,7 @@ import Test from './hooks/Test'
 import DataFetch from './hooks/DataFetch'
 import ProductFetch from './hooks/ProductFetch'
 import Main from './hooks/Main'
+import Show from './context/Show'
 
 const MyRoute = () => {
   return (
@@ -24,6 +25,9 @@ const MyRoute = () => {
         <Route path='/data' element={<DataFetch />} />
         <Route path='/products' element={<ProductFetch />} />
         <Route path='/main' element={<Main />} />
+
+        {/* context */}
+        <Route path='/context' element={<Show />} />
       </Routes>
     </Router>
   )
