25e5092939363c127692886cb98f1b0c8d753d5d
Nirajan Mahara <N.Mahara@wlv.ac.uk>
2022-01-05 13:13:09 +0545

    fix bug `no-unused-vars`: `useState, useEffect` is defined but never used

--- src/hooks/ProductFetch.js
+++ src/hooks/ProductFetch.js
@@ -4,10 +4,10 @@ import Product from './Product'
 
 const ProductFetch = () => {
   // useState for product data
-  const [products, setProducts] = React.useState([]) // setProducts is a function that takes in an array of products and sets the state to that array
+  const [products, setProducts] = useState([]) // setProducts is a function that takes in an array of products and sets the state to that array
 
   // useEffect for fetching data
-  React.useEffect(() => {
+  useEffect(() => {
     // Make a GET request for a products with a given URL
     axios
       .get('https://fakestoreapi.com/products') // the url we want to get data from
@@ -27,15 +27,21 @@ const ProductFetch = () => {
     // return the JSX
     // JSX is a syntax for writing HTML in Javascript
     <>
-      <div className='row'> {/* .row is a bootstrap class */}
-        {products.map((product) => (    // .map is a method that takes in an array and returns an array of JSX  // product is a variable that represents each item in the array of products // product is an object that represents each product in the array of products 
-          <Product  // Product is a component that we want to render
-              key={product.id}      // key is a property that tells React that this element should be unique  // product.id is the id of the product
+      <div className='row'>
+        {' '}
+        {/* .row is a bootstrap class */}
+        {products.map(
+          (
+            product // .map is a method that takes in an array and returns an array of JSX  // product is a variable that represents each item in the array of products // product is an object that represents each product in the array of products
+          ) => (
+            <Product // Product is a component that we want to render
+              key={product.id} // key is a property that tells React that this element should be unique  // product.id is the id of the product
               title={product.title} // title is a prop  // product.title is the title of the product
               image={product.image} // image is a prop  // product.image is the image of the product
               price={product.price} // price is a prop  // product.price is the price of the product
-            /> 
-        ))}
+            />
+          )
+        )}
       </div>
     </>
   )
